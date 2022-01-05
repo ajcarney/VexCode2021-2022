544e690bce093d440ce8bb33e4cc84787a15eb40
Nolan Fors <nolan.fors@hotmail.com>
2022-01-05 13:34:54 -0800

    Pid tuning + Config

--- RobotCode/src/Autons.cpp
+++ RobotCode/src/Autons.cpp
@@ -72,12 +72,12 @@ void Autons::pid_straight_drive() {
     chassis.set_okapi_sdrive_gains({1, 0.0001, 0, INT32_MAX, INT32_MAX});
 
     // chassis.pto_enable_drive();
-    // chassis.okapi_pid_straight_drive(1000, 7500, 1000, false, 0);
+    chassis.okapi_pid_straight_drive(1900, 7500, 1000, false, 0);
     // pros::delay(2000);
-    chassis.turn_right(90, 300, 2000, false); //Turn PID test
-    pros::delay(2000);
-    chassis.turn_left(90, 300, 2000, false); //Turn PID test
-    pros::delay(2000);
+    // chassis.turn_right(90, 300, 2000, false); //Turn PID test
+    // pros::delay(2000);
+    // chassis.turn_left(90, 300, 2000, false); //Turn PID test
+    // pros::delay(2000);
 
     //
     // chassis.pto_enable_rings();  // make sure to call this function before running rings otherwise rings will not run
@@ -145,26 +145,41 @@ void Autons::win_point() {
 }
 
 
-//ADDED BY NOLAN PENDING REVIEW
 void Autons::MidMogoLeft() {
-  PTOChassis chassis(Motors::front_left, Motors::front_right, Motors::back_left, Motors::back_right, Motors::mid_left, Motors::mid_right, Motors::piston3, Motors::piston4, Sensors::left_encoder, Sensors::right_encoder, 16, 3/5);
-  LiftController lift(Motors::lift1, Motors::lift2);
-  PositionTracker* tracker = PositionTracker::get_instance();
-  tracker->start_thread();
-  tracker->enable_imu();
-  tracker->set_log_level(0);
-  tracker->set_position({0, 0, 0});
-  chassis.set_turn_gains({1, 0.001, 20, INT32_MAX, INT32_MAX});
-  chassis.set_okapi_sdrive_gains({1, 0.0001, 0, INT32_MAX, INT32_MAX});
+    PTOChassis chassis(Motors::front_left, Motors::front_right, Motors::back_left, Motors::back_right, Motors::mid_left, Motors::mid_right, Motors::piston3, Motors::piston4, Sensors::left_encoder, Sensors::right_encoder, 16, 3/5);
+    LiftController lift(Motors::lift1, Motors::lift2);
+    PositionTracker* tracker = PositionTracker::get_instance();
+    tracker->start_thread();
+    tracker->enable_imu();
+    tracker->set_log_level(0);
+    tracker->set_position({0, 0, 0});
+    chassis.set_turn_gains({1, 0.001, 20, INT32_MAX, INT32_MAX});
+    chassis.set_okapi_sdrive_gains({1, 0.0001, 0, INT32_MAX, INT32_MAX});
 
+    // chassis.pto_enable_drive();
+    chassis.okapi_pid_straight_drive(1900, 7500, 1000, false, 0);
 
-// Middle Mogo LEFT
-    //Insert Piston Setpoints
-    chassis.okapi_pid_straight_drive(1900, 7500, 2000, false, 0); //Drives to middle left mogo
-    //Insert Claw Piston Clamp
-    // chassis.okapi_pid_straight_drive(-1000, 7000, 1000, false, 0); //Drives backwards to score middle mogo
-    //Insert Claw Piston Clamp
-    //Insert Lift Movement up
+    //
+    // chassis.pto_enable_rings();  // make sure to call this function before running rings otherwise rings will not run
+    // chassis.start_run_rings();
+    // pros::delay(1000);
+    // chassis.stop_run_rings();
+    //
+    // chassis.set_okapi_sdrive_gains({0.002, 0.0001, 0, INT32_MAX, INT32_MAX});  // change pid constants because driving backwards with less motors
+    // chassis.okapi_pid_straight_drive(-1000);  // this drives backwards without the extra motors because the enable drive function was not called
+    //
+    // chassis.pto_enable_drive();  // enable the extra motors again
+    // chassis.set_okapi_sdrive_gains({0.001, 0.0001, 0, INT32_MAX, INT32_MAX});  // change pid constants back to default because 6 motors are enabled again
+    //
+    //
+    // Motors::piston5.set_value(false);  // do something to the claw (idk if this is open or close)
+    // Motors::piston6.set_value(false);
+    //
+    // Motors::piston1.set_value(true);  // do something to the claw (idk if this is open or close)
+    // Motors::piston2.set_value(true);  // do something to the mogo (idk if this is open or close)
+    //
+//  chassis.turn_left(51.5, 300, 1000, false); //Turn PID test
+//  int uid = chassis.okapi_pid_straight_drive(1500, 10000, 6000, false, 0); //Drives foward to test pid_straight_drive
 }
 
 void Autons::MidMogoRight() {
